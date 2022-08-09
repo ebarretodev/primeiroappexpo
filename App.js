47e805bd137a322322069c344b9ac0f4b6137a44
Eliabel Barreto <67025189+ebarretodev@users.noreply.github.com>
2022-08-09 16:54:33 -0300

    Buttons and types for use

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { useState } from 'react';
-import { Linking, StyleSheet, Text, View, Button, TouchableOpacity, SafeAreaView, ScrollView, RefreshControl, Platform, StatusBar, FlatList, SectionList, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, SafeAreaView, Platform, StatusBar, TextInput, TouchableHighlight, TouchableWithoutFeedback, Pressable } from 'react-native';
 
 TouchableOpacity.defaultProps = { activeOpacity: 0.1 }; //used to disappears when clicked
 
 export default function App() {
 
-  const [name, setName] = useState('')
+  const [name, setName] = useState('teste')
   const [visible, setVisible] = useState(true)
+
+  const onPressHandler = () => {
+    setVisible(!visible)
+  }
 
   return (
     <SafeAreaView style={styles.safeArea} >
@@ -15,11 +19,59 @@ export default function App() {
         <TextInput
           style={styles.input}
           placeholder='e.g. John'
+          value={name}
           onChangeText={value => setName(value)}
           secureTextEntry={visible}da
         />
-        <Button title='Visible' onPress={()=>{setVisible(!visible)}} />
+
+        <Button
+          title={visible ? 'Show' : 'Hide' }
+          onPress={onPressHandler}
+
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={onPressHandler}
+        >
+          <Text style={styles.textButton}  >{visible ? 'Show' : 'Hide'}</Text>
+        </TouchableOpacity>
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={onPressHandler}
+          underlayColor="#ddd"
+        >
+          <Text style={styles.textButton}  >{visible ? 'Show' : 'Hide'}</Text>
+        </TouchableHighlight>
+
+        <TouchableWithoutFeedback
+          onPress={onPressHandler}
+        >
+          <View style={styles.button}>
+            <Text style={styles.textButton}  >{visible ? 'Show' : 'Hide'}</Text>
+          </View>
+        </TouchableWithoutFeedback>
+
+        <Pressable
+          style={({pressed}) => [
+            styles.button,
+            { backgroundColor: pressed ? '#ddd' : '#0f0' }
+          ]}
+          onPress={onPressHandler}
+          //onLongPress={onPressHandler}
+          delayLongPress={2000}
+          hitSlop={{ top: 10, bottom: 10, right: 10, left: 10 }}
+          android_ripple= {{color: '#00f'}}
+
+        >
+          <Text style={styles.textButton}  >{visible ? 'Show' : 'Hide'}</Text>
+        </Pressable>
+
+
         <Text style={styles.text} > Your name is {name}</Text>
+
+
       </View>
     </SafeAreaView>
   );
@@ -53,6 +105,21 @@ const styles = StyleSheet.create({
     fontSize: 25,
     padding: 10,
     textAlign: 'center',
+    margin: 10
 
+  },
+  button: {
+    backgroundColor: '#0f0',
+    width: 150,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+    borderRadius: 10,
+  },
+  textButton: {
+    color: '#fff',
+    fontSize: 15,
+    fontWeight: 'bold'
   }
 });
