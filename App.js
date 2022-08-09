77f464fc77d242d7cf0831a6181a0008cacef68a
Eliabel Barreto <67025189+ebarretodev@users.noreply.github.com>
2022-08-08 21:09:57 -0300

    This is a list using Safe Area View Carefully with android and ios solution for notch devices and use Scroll View to see every components

--- App.js
+++ App.js
@@ -1,104 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Linking, StyleSheet, Text, View, Button, TouchableOpacity, SafeAreaView } from 'react-native';
-
+import { Linking, StyleSheet, Text, View, Button, TouchableOpacity, SafeAreaView, ScrollView, RefreshControl, Platform, StatusBar } from 'react-native';
 
 TouchableOpacity.defaultProps = { activeOpacity: 0.1 }; //used to disappears when clicked
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState([
+    { key: 1, item: 'Item 1' },
+    { key: 2, item: 'Item 2' },
+    { key: 3, item: 'Item 3' },
+    { key: 4, item: 'Item 4' },
+    { key: 5, item: 'Item 5' },
+    { key: 6, item: 'Item 6' },
+    { key: 7, item: 'Item 7' },
+    { key: 8, item: 'Item 8' },
+    { key: 9, item: 'Item 9' },
+    { key: 27, item: 'Item 27' },
+    { key: 98, item: 'Item 98' },
+  ])
+  const [refreshing, setRefreshing] = useState(false)
 
+  const onRefresh = () => {
+    setRefreshing(true)
+    setItems([...items, { key: 69, item: 'Item 69' }])
+    setRefreshing(false)
+  }
 
 
   return (
-    <SafeAreaView style={styles.body}>
-      <View style={styles.viewLine1}>
-        <View style={styles.view1}>
-          <Text style={styles.text}>1</Text>
-        </View>
-        <View style={styles.view2}>
-          <Text style={styles.text}>2</Text>
-        </View>
-        <View style={styles.view3}>
-          <Text style={styles.text}>3</Text>
-        </View>
-      </View>
-      <View style={styles.view4}>
-        <Text style={styles.text}>4</Text>
-      </View>
-      <View style={styles.view5}>
-        <Text style={styles.text}>5</Text>
-      </View>
-      <View style={styles.viewLine2}>
-        <View style={styles.view6}>
-          <Text style={styles.text}>6</Text>
-        </View>
-        <View style={styles.view7}>
-          <Text style={styles.text}>7</Text>
-        </View>
-      </View>
-    </SafeAreaView>
-  );
+    <SafeAreaView style={styles.safeArea} >
+      <ScrollView
+        style={styles.body}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            colors={['#FF00FF']}
+          />
+        }
+      >
+        {
+          items.map((i) => {
+            return (
+              <View style={styles.item} key={i.key}>
+                <Text style={styles.text}>{i.item}</Text>
+              </View>
+            )
+          })
+        }
+        </ScrollView>
+        </SafeAreaView>
+        );
 }
 
 
 const styles = StyleSheet.create({
+  safeArea: {
+
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
   body: {
     flex: 1,
-    alignItems: 'stretch',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
   },
-  viewLine1: {
-    flexDirection: 'row',
-
-  },
-  viewLine2: {
-    flexDirection: 'row',
-    flex: 1,
-  },
-  view1: {
-    flex: 1,
+  item: {
+    backgroundColor: '#4ae1fa',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#5FF'
-  },
-  view2: {
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#E401E4'
-  },
-  view3: {
-    flex: 3,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F3F303'
-  },
-  view4: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f00'
-  },
-  view5: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#0f0'
-  },
-  view6: {
-    flex:1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff'
-  },
-  view7: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#00f'
+    margin: 10
   },
   text: {
     color: '#000',
-    fontSize: 25,
+    fontSize: 45,
     fontStyle: 'italic',
     margin: 10,
   },
